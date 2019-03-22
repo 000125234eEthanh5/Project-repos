41f5e6cd13dd1673e0dc084e1921accdd46604f3
000125234eEthanh5 <32342571+000125234eEthanh5@users.noreply.github.com>
2019-03-21 23:46:20 -0600

    this isn't going super well...

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -2,11 +2,43 @@
 
 namespace RockPaperScissors
 {
-    class Program
+    class RockPaperScissorsGame
     {
+         private Random rng;
+            private int wins;
+            private int losses;
+            private int ties;
+            public RockPaperScissorsGame()
+            {
+                rng = new Random();
+            }
+            public void Play()
+            {
+                string userChoice;
+                userChoice = PromptUser();                
+            }
+            private string PromptUser()
+            {
+                while (true)
+                {
+                Console.Write("Enter your choice: (R)ock, (P)aper, (S)cissors, or (Q)uit: ");
+                string choice = Console.ReadLine();
+
+                if (choice == "R" || choice == "P" || choice == "S" || choice == "Q")
+                {
+                    return choice;
+                }
+                else
+                {
+                Console.WriteLine("That was not a valid choice!");
+                }
+                }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+                
         }
     }
 }
+}
+
